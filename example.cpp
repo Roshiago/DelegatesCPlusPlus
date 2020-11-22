2a6b6c772272da596c362e0544f3285282c81a61
Maxim <redko10maksim@gmail.com>
2020-11-22 05:04:59 +0300

    add example for delegate with class method and func

--- example.cpp
+++ example.cpp
@@ -6,6 +6,10 @@ int print(const char* str) {
     return 0;
 }
 
+int valueBy6(int i) {
+    return i * 6;
+}
+
 class C {
 public:
     int getValue(int i) {
@@ -44,7 +48,21 @@ int main(){
     Will be printed: 
         5
         50
-        
+
+    */
+    for (std::list<int>::iterator i = results.begin(); i != results.end(); i++) {
+        std::cout << *i << std::endl;
+    }
+
+    CDelegate<int, int> delegClassAndFunc;
+    delegClassAndFunc += WrapMethod<int, B, getValueBy10, int>(&b, &B::getValueBy10);
+    delegClassAndFunc += WrapFunc<int, int(*)(int), int>(valueBy6);
+    results = delegClassAndFunc(5);
+    /*
+    Will be printed:
+        50
+        30
+
     */
     for (std::list<int>::iterator i = results.begin(); i != results.end(); i++) {
         std::cout << *i << std::endl;
