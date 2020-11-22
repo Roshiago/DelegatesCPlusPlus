e1064f6e41d503c314ea012415010e2e7710e299
Maxim <redko10maksim@gmail.com>
2020-11-22 05:01:49 +0300

    add example for delegate with class method

--- example.cpp
+++ example.cpp
@@ -6,6 +6,24 @@ int print(const char* str) {
     return 0;
 }
 
+class C {
+public:
+    int getValue(int i) {
+        return i;
+    }
+};
+
+typedef int(C::* getValue)(int);
+
+class B {
+public:
+    int getValueBy10(int i) {
+        return i*10;
+    }
+};
+
+typedef int(B::* getValueBy10)(int);
+
 int main(){
     CDelegate<int, const char*> deleg;
     deleg += WrapFunc<int, int(*)(const char*), const char*>(print);
@@ -15,5 +33,22 @@ int main(){
     Will be printed HELLO WORLD 3 times
     */
     deleg("HELLO WORLD");
+
+    CDelegate<int, int> delegClass;
+    C a;
+    B b;
+    delegClass += WrapMethod<int, C, getValue, int>(&a, &C::getValue);
+    delegClass += WrapMethod<int, B, getValueBy10, int>(&b, &B::getValueBy10);
+    std::list<int> results = delegClass(5);
+    /*
+    Will be printed: 
+        5
+        50
+        
+    */
+    for (std::list<int>::iterator i = results.begin(); i != results.end(); i++) {
+        std::cout << *i << std::endl;
+    }
+
     return 0;
 }
